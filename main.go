0a01f3df068be42cf5da19dd855dcd75255b7945
VincentRet <115279323+VincentRet@users.noreply.github.com>
2025-05-09 00:19:12 -0400

    Added Godoc comments to Add

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
-// add.go
+// Package mathmod provides basic math utilities.
 package mathmod
 
 // Add returns the sum of a and b.
+//
+// See also: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
-	return a + b
+    return a + b
 }
