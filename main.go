4ac65a4bfa31791f5f3ed00c7fe72a0af4492ce1
VincentRet <115279323+VincentRet@users.noreply.github.com>
2025-05-09 00:23:20 -0400

    Added generic Add function with constraints

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 // Package mathmod provides basic math utilities.
 package mathmod
 
+import "golang.org/x/exp/constraints"
+
+// Number is a type constraint for numeric types.
+type Number interface {
+    constraints.Integer | constraints.Float
+}
+
 // Add returns the sum of a and b.
 //
 // See also: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
     return a + b
 }
